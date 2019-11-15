3424abe32f71823c7dc2c0504f42cc9fe185af4b
Aleksandar Krstic <aleksandar.krs17@gmail.com>
2019-11-15 20:59:17 +0100

    Odradjen Landing Page

---
+++ js/main.js
@@ -0,0 +1,12 @@
+// Ovde treba da stoji f-ja koja nam omogucava da se igramo sa menijem
+function myFunction(x) {
+    x.classList.toggle("change");
+    x.classList.toggle("overlay.active");
+
+    var menu = document.getElementsByName('menu-first');
+
+    console.log('Radi');
+
+    menu.classList.add('after-click');
+}
+
