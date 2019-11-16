3e4ae984331213d8fc2ee7d745a3fb3301fd64b8
Aleksandar Krstic <aleksandar.krs17@gmail.com>
2019-11-16 13:04:29 +0100

    Azuriran JS

--- js/main.js
+++ js/main.js
@@ -1,15 +1,3 @@
-// Ovde treba da stoji f-ja koja nam omogucava da se igramo sa menijem
-function myFunction(x) {
-    x.classList.toggle("change");
-    x.classList.toggle("overlay.active");
-
-    var menu = document.getElementsByName('menu-first');
-
-    console.log('Radi');
-
-    menu.classList.add('after-click');
-}
-
 $(document).ready(function(){
     $('.toggle').click(function(){
         $('.toggle').toggleClass('active');
