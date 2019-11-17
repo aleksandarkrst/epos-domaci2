ce5aceeec2f06a29be51e76b4eb65b32931e6247
Aleksandar Krstic <aleksandar.krs17@gmail.com>
2019-11-17 22:33:43 +0100

    Popravljen Meni

--- js/main.js
+++ js/main.js
@@ -2,6 +2,6 @@ $(document).ready(function(){
     $('.toggle').click(function(){
         $('.toggle').toggleClass('active');
         $('.overlay').toggleClass('active');
-        $('.navbar').toggleClass('active');
+        $('.menu').toggleClass('active');
     });
 });
