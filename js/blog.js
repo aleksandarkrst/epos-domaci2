4c966c38ee2d57fc4c86dda107bb08214507a922
mmaric005 <maricmasa@gmail.com>
2019-11-19 23:02:09 +0100

    upload

--- js/blog.js
+++ js/blog.js
@@ -1,10 +1,10 @@
-$(document).ready(function(){
-    $('.toggle').click(function(){
-        $('.toggle').toggleClass('active');
-        $('.overlay').toggleClass('active');
-        $('.navbar').toggleClass('active');
-    });
-    
+$(document).ready(function () {
+  $('.toggle').click(function () {
+    $('.toggle').toggleClass('active');
+    $('.overlay').toggleClass('active');
+    $('.navbar').toggleClass('active');
+  });
+
 });
 var modal = document.getElementById("myModal");
 
@@ -15,18 +15,39 @@ var btn = document.getElementById("myBtn");
 var span = document.getElementsByClassName("close")[0];
 
 // When the user clicks on the button, open the modal
-btn.onclick = function() {
+btn.onclick = function () {
   modal.style.display = "block";
 }
 
 // When the user clicks on <span> (x), close the modal
-span.onclick = function() {
+span.onclick = function () {
   modal.style.display = "none";
 }
 
 // When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
+window.onclick = function (event) {
   if (event.target == modal) {
     modal.style.display = "none";
   }
+}
+
+
+function popUp() {
+  $('.popup-with-form').magnificPopup({
+    type: 'inline',
+    preloader: false,
+    focus: '#test-form',
+
+    // When elemened is focused, some mobile browsers in some cases zoom in
+    // It looks not nice, so we disable it:
+    callbacks: {
+      beforeOpen: function () {
+        if ($(window).width() < 700) {
+          this.st.focus = false;
+        } else {
+          this.st.focus = '#name';
+        }
+      }
+    }
+  });
 }
