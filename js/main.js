e73e1583167948e49d6b057a7456c00d8ffcc622
Aleksandar Krstic <aleksandar.krs17@gmail.com>
2019-11-20 00:38:50 +0100

    Loader i typewritter efekt

--- js/main.js
+++ js/main.js
@@ -1,7 +1,19 @@
-$(document).ready(function(){
-    $('.toggle').click(function(){
+$(document).ready(function () {
+    $('.toggle').click(function () {
         $('.toggle').toggleClass('active');
         $('.overlay').toggleClass('active');
         $('.menu').toggleClass('active');
     });
 });
+
+var myVar;
+
+function myFunction() {
+    myVar = setTimeout(showPage, 2000);
+}
+
+function showPage() {
+    console.log('Radi');
+    $('.load').toggleClass('active');
+    $('.loader').toggleClass('active');
+}
